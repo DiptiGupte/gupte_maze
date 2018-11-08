00688c3a294b9a4c0f4db4400d65a8fd43bc2b82
Dipti Gupte <gupte.d50@gmail.com>
2018-11-08 09:48:23 -0800

    added background and point system

--- Assets/scripts/pickup.cs
+++ Assets/scripts/pickup.cs
@@ -6,29 +6,39 @@ public class pickup : MonoBehaviour {
 
     public float speed;
     private Rigidbody rb;
+    private int bugCount;
+    private int compCount;
+    private int buttonCount;
 
 	// Use this for initialization
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        bugCount = 0; 
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVeritcal = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVeritcal);
-
-        rb.AddForce(movement * speed);
+        
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("bug_dipti"))
+        if (other.gameObject.CompareTag("pickup"))
         {
             other.gameObject.SetActive(false);
+            bugCount = bugCount + 5;
+        }
+        else if (other.gameObject.CompareTag("comp"))
+        {
+            other.gameObject.SetActive(false);
+            compCount = compCount + 10;
+        }
+        else if (other.gameObject.CompareTag("button"))
+        {
+            other.gameObject.SetActive(false);
+            buttonCount = buttonCount + 1;
         }
     }
 }
